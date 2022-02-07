c198774f82736245ac6093238af7738530c3f8d3
abdoulayeniang2010 <abdoulayeniang2010@gmail.com>
2022-02-07 23:46:56 +0000

    initisation matrix

--- dijkstra.py
+++ dijkstra.py
@@ -11,9 +11,11 @@ class Dijkstra():
 			self.node = node
 			self.size_edge = 0			
 			if input_node in node:
+				self.check_input_node = 1
 				self.input_node = input_node
-				self.node_visited = np.full((len(node), len(node)), 0)
+				self.node_visited = np.full((len(node)), 0)
 			else:
+				self.check_input_node = 0
 				print ("WARNING : Le sommet d'entree n'est pas sur la liste des sommets !!!!\n")
 		else:
 			self.check = 0
@@ -35,7 +37,30 @@ class Dijkstra():
 			
 		else:
 			print(self.check_text)
+	
+	
+	def initialisation (self):
 
+		if self.check_input_node == 1:
+			i = self.node.index(self.input_node)
+			self.current_matrix = np.full( (self.size_matrix, self.size_matrix+1),"nan" )
+			
+			for line in range(self.size_matrix):
+				for row in range(self.size_matrix):
+					if line == 0 and row == i:
+						self.current_matrix[0][i] = 0
+						self.node_visited[i] = 1
+						self.current_matrix[0][self.size_matrix] = int(i)
+					else:
+						self.current_matrix[line][row] = float('inf')
+			output = self.current_matrix
+		else:
+			output = "Warning : Le sommet initiale n'est pas sur la liste"
+		
+		return output
+	
+
+# Test 
 matrix = np.array([
 	[0., 5., 2., 6., 0., 0., 0.],
 	[0., 0., 2., 0., 5., 0., 0.],
@@ -48,5 +73,5 @@ matrix = np.array([
 
 node = ("A","B","C","D","E","F","G")
 D = Dijkstra(matrix, node, 'A')
-D.desc()
-
+#D.desc()
+print(D.initialisation() )
